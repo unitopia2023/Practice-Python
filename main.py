f40ad9cb2b52bb53d408d649bd49ca990b5bf39d
unitopia2023@gmail.com <your_email@example.com>
2024-10-15 13:00:05 +0800

    practice commit 2

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# def add(a, b):
+#     return a + b
+
+# def subtract(a, b):
+#     return a - b
+
+# # Example usage
+# num1 = 10
+# num2 = 5
+# print(f"Addition: {add(num1, num2)}")
+# print(f"Subtraction: {subtract(num1, num2)}")
  
+
+
+
+
+
+
+n = input('name: ')
+print(n)
